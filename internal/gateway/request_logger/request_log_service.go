186a5270329894bbb0b1fc233652f9c9729e8af3
Filip Jovanov <filip.jovanov@students.finki.ukim.mk>
2025-11-06 21:43:46 +0100

    feat(request_logs): Add extra logging for debugging

--- internal/gateway/request_logger/request_log_service.go
+++ internal/gateway/request_logger/request_log_service.go
@@ -1,19 +1,28 @@
 package requestlogger
 
-import "github.com/PegasusMKD/svedprint-go/internal/gateway/db/sqlc"
+import (
+	"fmt"
+
+	"github.com/PegasusMKD/svedprint-go/internal/gateway/db/sqlc"
+	"github.com/PegasusMKD/svedprint-go/pkg/logger"
+	"github.com/rs/zerolog"
+)
 
 type RequestLogService struct {
 	repository *RequestLogRepository
+	logger     *zerolog.Logger
 }
 
 func NewRequestLogService(repository *RequestLogRepository) *RequestLogService {
 	return &RequestLogService{
 		repository: repository,
+		logger:     logger.Get(),
 	}
 }
 
 func (service *RequestLogService) CreateLogs(logs []sqlc.RequestLog) {
 	data := make([]sqlc.BatchInsertRequestLogsParams, 100)
+	service.logger.Println(fmt.Sprintf("Received this data %v", data))
 
 	for idx, log := range logs {
 		data[idx] = sqlc.BatchInsertRequestLogsParams{
@@ -29,6 +38,8 @@ func (service *RequestLogService) CreateLogs(logs []sqlc.RequestLog) {
 			ErrorMessage:    log.ErrorMessage,
 		}
 	}
+
+	service.logger.Println(fmt.Sprintf("Attempting to save %v", data))
 
 	service.repository.InsertBatch(data)
 }
